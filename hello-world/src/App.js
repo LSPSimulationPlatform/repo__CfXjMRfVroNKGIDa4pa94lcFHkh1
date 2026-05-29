d9e7508aa708b68fa73eb9f533f794e01b3d794f
AynurShukur <96521946+AynurShukur@users.noreply.github.com>
2026-05-29 17:16:41 +0000

    my first project

---
+++ hello-world/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <label style={{fontSize:"30px", color:"red"}}>It's my first react project</label>
+    </div>
+  );
+}
+
+export default App;
